cf2245cc8f6109677a026d283056412ee5fe514c
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-11 00:42:15 -0500

    DB Up phase 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,14 +11,47 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Define Schema (Skeleton)
+const itemSchema = {
+  name: String
+};
+
+// Create Model
+const Item = mongoose.model("Item", itemSchema);
+
+// Populate Model
+const amanda = new Item({
+  name: "Amanda"
+});
+
+const projectAmaani = new Item({
+  name: "Amaani"
+});
+
+const america = new Item({
+  name: "America"
+});
+
+const defaultItems = [amanda, projectAmaani, america];
+
+Item.find({}, function(err, foundItems){
+  if(foundItems.length === 0){
+    Item.insertMany(defaultItems, function(err){
+      console.log("Become as Gods!");
+    })
+  }
+
+  else{
+    console.log("default items already present");
+  }
+});
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: defaultItems});
 
 });
 
@@ -34,6 +67,8 @@ app.post("/", function(req, res){
     res.redirect("/");
   }
 });
+
+
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
