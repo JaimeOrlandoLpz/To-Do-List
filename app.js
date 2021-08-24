0f7cf199643d65ebf1a5a4524494627bab2cbe88
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-24 18:52:33 -0500

    Atlas Implementation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-jaime:test123@cluster0.fatni.mongodb.net/todolist", {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Define Schemas (Skeleton)
 const itemSchema = {
