f559b0d54a2c4611cbe563d18a6384f4d103f698
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-25 15:40:51 -0500

    heroku

--- app.js
+++ app.js
@@ -148,7 +148,12 @@ app.get("/:customListName", function(req, res){
 
 });
 
+let port = process.env.port;
+if(port == null || port == ""){
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server started");
 });
